af9e1aee4241140d86154a6f0f923638b11122f8
Yash Govani <dhvyas143@gmail.com>
2023-12-03 23:04:14 +0530

    feat: error handling implementing using express

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 // const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,10 +9,13 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 character'],
+      minlength: [10, 'A tour name must have more or equal than 10 character'],
+      // validate: [validator.isAlpha, 'Tour name must only contain character'],
     },
     duration: {
       type: Number,
-      // required: [true, 'A tour must have duration'],
+      required: [true, 'A tour must have duration'],
     },
     maxGroupSize: {
       type: Number,
@@ -20,17 +24,32 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
       default: 4.5,
     },
     price: { type: Number, required: [true, 'A tour must have price'] },
-    priceDiscount: { type: Number },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (value) {
+          // this only points to current doc on NEW document creation
+          return value < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
